6016bb10358ab5bc38ab25a2f3725458de4ff1d7
zyleq <thompsd11@gmail.com>
2023-06-27 22:43:39 -0400

    updated tests

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -53,11 +53,11 @@ class AddTodo extends Component {
       // 4. The value of the text field also should reflect the local state of this component.
       <div>
         <TextField
+          data-testid="new-item-input"
           label="Add New Item"
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
-          data-testid="new-item-input"
         />
         <LocalizationProvider dateAdapter={AdapterDateFns}>         
       <DesktopDatePicker
@@ -69,11 +69,11 @@ class AddTodo extends Component {
        />
         </LocalizationProvider>
         <Button
+          data-testid="new-item-button"
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
           variant="contained"
           color="primary"
-          data-testid="new-item-button"
         >
           Add
         </Button>
