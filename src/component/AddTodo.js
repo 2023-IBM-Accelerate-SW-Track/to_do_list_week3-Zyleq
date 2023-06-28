ac6d31758cd082b04a151dfad92e64ca90b4d8be
zyleq <thompsd11@gmail.com>
2023-06-27 22:08:37 -0400

    updated tests

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -57,6 +57,7 @@ class AddTodo extends Component {
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
+          data-testid="new-item-input"
         />
         <LocalizationProvider dateAdapter={AdapterDateFns}>         
       <DesktopDatePicker
@@ -72,6 +73,7 @@ class AddTodo extends Component {
           onClick={this.handleSubmit}
           variant="contained"
           color="primary"
+          data-testid="new-item-button"
         >
           Add
         </Button>
